72989265912269d0ffcbca8d772d867e3f8fd0ce
Anjana Tissera <anjanat@ait.asia>
2026-02-02 15:42:26 +0700

    Completed lesson 124

---
+++ mycv/test/auth.e2e-spec.ts
@@ -0,0 +1,50 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { App } from 'supertest/types';
+import { AppModule } from './../src/app.module';
+
+describe('Authentication System', () => {
+  let app: INestApplication<App>;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('handles a signup request', () => {
+    const email = 'testxx@gmail.com';
+
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email: email, password: 'test' })
+      .expect(201)
+      .then((res) => {
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toEqual(email);
+      });
+  });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'testxx@gmail.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email: email, password: 'test' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie') as string[];
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
+  });
+});
